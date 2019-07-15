b91ea07cde9982f1dabebae2e6100c2c97d150fb
Matthew5 <jnwu@thoughtworks.com>
2019-07-15 22:24:04 +0800

    contract: add user service

---
+++ src/type/user.d.ts
@@ -0,0 +1,20 @@
+interface UserInfo {
+  nickName: string
+  avatarUrl: string
+  gender: number
+  province: string
+  city: string
+  country: string
+}
+
+interface GetUserInfoReq {
+  code: string
+}
+
+interface GetUserInfoRes extends UserInfo {
+  userId: number
+}
+
+interface UpdateUserInfoReq extends UserInfo {
+  userId?: number
+}
