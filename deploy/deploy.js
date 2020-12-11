1ba3e5dc1dc8ff5af57be86d21b04cad8d04b5dc
Matthew5 <jnwu@thoughtworks.com>
2020-12-11 23:28:10 +0800

    add mp ci

---
+++ deploy/deploy.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const ci = require('miniprogram-ci')
+const commander = require('commander')
+
+function getProject(privateKeyPath) {
+  const projectPath = path.resolve(__dirname, '../dist')
+  const projectConfig = require(path.resolve(projectPath, './project.config.json'))
+
+  return new ci.Project({
+    projectPath,
+    privateKeyPath,
+    appid: projectConfig.appid,
+    type: projectConfig.compileType
+  })
+}
+
+function displayPackageInfo({ subPackageInfo }) {
+  console.log('------------------Package info--------------')
+  subPackageInfo.forEach(({ name, size }, index) => {
+    console.log(`Package: ${name}`)
+    console.log(`Size: ${size}`)
+    if (subPackageInfo.length !== (index + 1)) {
+      console.log('-----------------')
+    }
+  })
+  console.log('------------------Package info--------------')
+}
+
+async function deploy(command, args) {
+  let options = Object.keys(command.opts())
+    .reduce((memo, name, index) => ({...memo, [name]: args[index]}), {});
+
+  const { privateKey } = options;
+  if (!privateKey) {
+    throw new Error('--private-key is required')
+  }
+
+  const version = options.versions || Date.now()
+  const description = options.descriptions || 'Not Provide.'
+  const project = getProject(privateKey)
+
+  options = {
+    project,
+    version,
+    desc: description,
+    setting: {
+      es6: true
+    },
+    onProgressUpdate: () => {}
+  }
+
+  const [, uploaded] = await Promise.all([
+    ci.preview({
+      ...options,
+      qrcodeFormat: 'terminal',
+      qrcodeOutputDest: path.resolve(__dirname, './preview'),
+    }),
+    ci.upload({...options})]
+  );
+
+  displayPackageInfo(uploaded)
+}
+
+/**
+ * 上传代码
+ * node deploy.js deploy --versions dev-master --descriptions message --private-key ./private.key
+ */
+
+commander.program
+  .command('deploy')
+  .allowUnknownOption(true)
+  .option('--versions', '小程序版本')
+  .option('--descriptions', '小程序描述')
+  .option('--private-key', '密钥，用于代码上传')
+  .description('上传小程序代码')
+  .action((...args) => {
+    deploy(...args).catch(e => {
+      setTimeout(() => {
+        throw e
+      })
+    })
+  })
+
+commander.program.parse(process.argv)
---
+++ jenkinsfile.groovy
@@ -0,0 +1,59 @@
+pipeline {
+  agent none
+  environment {
+    CI_COMMIT_SHA = 'ci_commit_hash'
+    COMMIT_CHECKOUT = 'commit_checkout_version'
+  }
+  options {
+    buildDiscarder logRotator(artifactDaysToKeepStr: '50', artifactNumToKeepStr: '50', daysToKeepStr: '90', numToKeepStr: '5')
+    timeout(time: 1, unit: 'DAYS')
+  }
+
+  stages {
+    stage('Setup') {
+      steps {
+        setup()
+      }
+    }
+
+    stage('Deploy') {
+      environment {
+        WX_UPLOAD_SECRET = credentials('WX_UPLOAD_SECRET')
+      }
+
+      steps {
+        build()
+        deploy()
+      }
+    }
+  }
+}
+
+def info() {
+  sh 'node -v'
+  sh 'npm -v'
+  sh 'yarn -v'
+  sh 'env'
+}
+
+def setup() {
+  info()
+  CI_COMMIT_SHA = sh(script: "git log -n 1 --pretty=format:'%h'", returnStdout: true).trim()
+  COMMIT_CHECKOUT = GIT_COMMIT
+
+  sh 'yarn config set registry http://registry.npm.taobao.org'
+  sh 'yarn install'
+}
+
+def build() {
+  sh "rm -rf ./dist"
+  sh 'yarn build:prod'
+}
+
+def deploy() {
+  String branch = """${GIT_BRANCH.replace('origin/', '').replace('/', '')}"""
+  String version = "${branch}.${CI_COMMIT_SHA}"
+  String description = sh(returnStdout: true, script: 'git log --pretty=format:"[%h][%an] (%s)" -1').trim().replaceAll("\"", "")
+
+  sh """node deploy/deploy.js deploy --versions \"${version}\" --descriptions \"${description}\" --private-key ${WX_UPLOAD_SECRET}"""
+}
