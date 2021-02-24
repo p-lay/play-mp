dc2e0ebd27001afce7ba364812a5821e1cd848fc
Matthew5 <jnwu@thoughtworks.com>
2021-02-24 15:15:18 +0800

    upload depoly3

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -56,9 +56,9 @@ def buildPkg() {
 }
 
 def deploy() {
-  // String branch = """${GIT_BRANCH.replace('origin/', '').replace('/', '')}"""
-  // String version = "${branch}.${CI_COMMIT_SHA}"
+  String branch = """${GIT_BRANCH.replace('origin/', '').replace('/', '')}"""
+  String version = "${branch}.${CI_COMMIT_SHA}"
   String description = sh(returnStdout: true, script: 'git log --pretty=format:"[%h][%an] (%s)" -1').trim().replaceAll("\"", "")
 
-  sh """node deploy/deploy.js deploy --versions \"${MP_VERSION}\" --descriptions \"${description}\" --robot \"2\" --private-key ${WX_UPLOAD_SECRET}"""
+  sh """node deploy/deploy.js deploy --versions \"${version}\" --descriptions \"${description}\" --robot \"2\" --private-key ${WX_UPLOAD_SECRET}"""
 }
